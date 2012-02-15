38e44af2249f1810d75822a8191700c58fa2861d
jmaldonado <juan.maldonado@gmail.com>
2012-02-15 08:54:39 -0800

    Commiting all files before I update .gitignore and start ignoring all the secure stuff.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Splinter::Application.config.secret_token = 'fd8bd46039ca79939b255afbf9a0c59c70f18d9df9c055b36f0aeef4685ab1da8cdf5f7399dd40fd8ccd3e9a7826a37d22c6719f6e9f7e46cd2e3463a324775d'
